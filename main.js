520cb861269e0ee388ea76d901a7261ea1855b40
= <=>
2020-12-31 15:25:58 +0300

    tray1

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
-const getShows = ()=>{
-    axios
-    .get("http://api.tvmaze.com/shows")
+/*<div class="card">
+  <p id="name1">1</p>
+  <img id="image_1" src="" alt="">
+</div>*/
+
+//const getShows = () => {
+    var i=1
+    
+
+      axios
+
+    .get('http://api.tvmaze.com/shows/1')
     .then((response) => {
-      console.log(response)
-      const name_1 = response.data.results[0].name
-
-      const image1 = response.data.results[0].image
-
-      const name1 = $("#name1").text(name_1)
-      const image_1 = $("#image_1").attr("src",image1)
-
-
+        console.log(response);
+        const name_1 = response.data.name;
+        const image1 = response.data.image.medium;
+        const link_1=response.data.url
+        
+        $(`#name${i}`).text(name_1);
+        $(`#image_${i}`).attr("src", image1);
+        $(`#link${i}`).attr("href",link_1)
+        console.log(name_1);
+        console.log(image1);
+      
     })
     .catch((err) => {
-      console.log('ERR: ', err);
+      console.log("ERR: ", err);
     });
 
-}
+    axios
+    .get('http://api.tvmaze.com/shows/2')
+    .then((response) => {
+        console.log(response);
+        const name_2 = response.data.name;
+        const image2 = response.data.image.medium;
+        $(`#name2`).text(name_2);
+        $(`#image_2`).attr("src", image2);
+       
+      
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+    });
+    
+//};
+
+
+//////
